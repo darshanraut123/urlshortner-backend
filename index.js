61bf10304dc2d17b167b8479c0507af0af1d992e
Darshan Raut <56134448+darshanraut123@users.noreply.github.com>
2022-03-08 01:09:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 const cors = require("cors");
 
 app.use(express.static(path.join(__dirname, "public")));
